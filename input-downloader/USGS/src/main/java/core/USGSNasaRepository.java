f0fd5006f8ad8caacc1bd7847270f609b54276ae
Gustavo Yamaguchi <gustavoyama@gmail.com>
2017-10-09 09:32:31 -0300

    changing executables path

--- input-downloader/USGS/src/main/java/core/USGSNasaRepository.java
+++ input-downloader/USGS/src/main/java/core/USGSNasaRepository.java
@@ -171,7 +171,7 @@ public class USGSNasaRepository implements Repository {
     }
 
     private void runGetStationData(String collectionTierName, String localImageFilePath) throws IOException, InterruptedException {
-        ProcessBuilder builder = new ProcessBuilder("/home/ubuntu/execs/get-station-data.sh", collectionTierName, localImageFilePath);
+        ProcessBuilder builder = new ProcessBuilder("/home/ubuntu/get-station-data.sh", collectionTierName, localImageFilePath);
         LOGGER.info("Starting get station data script.");
         try {
             Process p = builder.start();
