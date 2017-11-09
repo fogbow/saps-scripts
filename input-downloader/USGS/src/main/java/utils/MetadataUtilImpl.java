2e4d645f003fb0a62577726d6d0b5ca4454c5664
EsdrasVP <esdras015@gmail.com>
2017-11-09 09:23:16 -0300

    Fixing test for input downloader metadata generation

--- input-downloader/USGS/src/main/java/utils/MetadataUtilImpl.java
+++ input-downloader/USGS/src/main/java/utils/MetadataUtilImpl.java
@@ -4,6 +4,7 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Date;
 
 import org.apache.log4j.Logger;
 
@@ -14,9 +15,13 @@ public class MetadataUtilImpl implements MetadataUtil {
 	@Override
 	public void writeMetadata(String inputDirPath, File metadataFile) throws IOException {
 		BufferedWriter writer = null;
+		Date date = new Date();
 
 		try {
 			writer = new BufferedWriter(new FileWriter(metadataFile));
+			writer.write("# Input Downloader Implementation Metadata\n");
+			writer.write("#" + date + "\n");
+			
 			writeInputFilesPaths(inputDirPath, writer);
 			LOGGER.info("Metadata file " + metadataFile.getAbsolutePath() + " generated");
 		} finally {
@@ -40,7 +45,7 @@ public class MetadataUtilImpl implements MetadataUtil {
 			throws IOException {
 		for (int i = 1; i <= 11; i++) {
 			if (inputFile.getName().endsWith("B" + i + ".TIF")) {
-				writer.write(inputFile.getAbsolutePath() + " # Scene band " + i + " file path");
+				writer.write(inputFile.getAbsolutePath() + " # Scene band " + i + " file path\n");
 			}
 		}
 	}
@@ -48,28 +53,28 @@ public class MetadataUtilImpl implements MetadataUtil {
 	protected void checkMTLAndPutInProperties(File inputFile, BufferedWriter writer)
 			throws IOException {
 		if (inputFile.getName().endsWith("MTL.txt")) {
-			writer.write(inputFile.getAbsolutePath() + " # Scene metadata file path");
+			writer.write(inputFile.getAbsolutePath() + " # Scene metadata file path\n");
 		}
 	}
 
 	protected void checkGCPAndPutInProperties(File inputFile, BufferedWriter writer)
 			throws IOException {
 		if (inputFile.getName().endsWith("GCP.txt")) {
-			writer.write(inputFile.getAbsolutePath() + " # Scene GCP file path");
+			writer.write(inputFile.getAbsolutePath() + " # Scene GCP file path\n");
 		}
 	}
 
 	protected void checkREADMEAndPutInProperties(File inputFile, BufferedWriter writer)
 			throws IOException {
 		if (inputFile.getName().startsWith("README")) {
-			writer.write(inputFile.getAbsolutePath() + " # Complete data description file path");
+			writer.write(inputFile.getAbsolutePath() + " # Complete data description file path\n");
 		}
 	}
 
 	protected void checkStationAndPutInProperties(File inputFile, BufferedWriter writer)
 			throws IOException {
 		if (inputFile.getName().endsWith("station.csv")) {
-			writer.write(inputFile.getAbsolutePath() + " # Meteorological data file path");
+			writer.write(inputFile.getAbsolutePath() + " # Meteorological data file path\n");
 		}
 	}
 }
