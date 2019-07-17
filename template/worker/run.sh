2b6104137d1c909efca3bd51a6c13b21676ea9bf
thiagowhispher <thiago.souza@ccc.ufcg.edu.br>
2019-07-17 12:03:11 -0300

    Worker template

--- template/worker/run.sh
+++ template/worker/run.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-#Objetive: Run the algorithm for the input data downloaded and/or preprocessed
-#For example: $0 /tmp/input /tmp/output /tmp/preprocessing /tmp/metadata
+## Objetive: Run the algorithm for the input data downloaded and/or preprocessed
+## For example: $0 /tmp/input /tmp/output /tmp/preprocessing /tmp/metadata
 
-#Checking args
+## Checking args
 if [ $# -ne 4 ]
 then
   echo "Usage: $0 input_path output_path preprocessing_path metadata_path"
   exit 1
 fi
 
-#Capture args
+## Capture args
 INPUTPATH=$1
 OUTPUTPATH=$2
 PREPROCESSINGPATH=$3
@@ -20,7 +20,7 @@ METADATAPATH=$4
 
 
 
-# Exit code
-The `run.sh` script should have the following return pattern:
-- `0` represents a successful execution.
-And any other exit code will be considered failed.
+## Exit code
+# The `run.sh` script should have the following return pattern:
+# - `0` represents a successful execution.
+# And any other exit code will be considered failed.
